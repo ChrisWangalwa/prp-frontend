ab548c121f694a8bea28e74bf8fc44fe3f488de8
Chris Wangalwa <wangalwac14@gmail.com>
2025-06-20 12:18:00 +0300

    Fix Firebase config typo (storageBucket)

--- firebase.js
+++ firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "XXXXXXXXXXXX",
-  appId: "APP_ID"
+  apiKey: "AIzaSyDM9iJezoZ5KZMIURIU_6ideS2i-_3lkeQ",
+  authDomain: "press-release-portal.firebaseapp.com",
+  projectId: "press-release-portal",
+  storageBucket: "press-release-portal.appspot.com",
+  messagingSenderId: "299194408268",
+  appId: "1:299194408268:web:36560a4ed8613c7cf9c3e9",
+  measurementId: "G-7RT348QSWT"
 };
 
 const app = initializeApp(firebaseConfig);
